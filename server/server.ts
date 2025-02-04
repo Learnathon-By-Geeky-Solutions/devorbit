83461a428d93cd4164c452a4d598d8fc4629ed34
navid-kamal <navidkamal568@gmail.com>
2025-02-04 12:59:15 +0600

    feat: add error handler middleware

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import connectDB from './config/db';
 const app = express();
 const port = 3000;
 
+connectDB();
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello000 World!');
 });
